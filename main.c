63602b5d2ace1e2a66f658072ee8fe2d1957d8a0
BK Oussama <oussama.boukoutaya@gmail.com>
2024-03-26 16:29:34 +0000

    Update main.c

--- main.c
+++ main.c
@@ -13,89 +13,56 @@
 #include "ft_printf.h"
 #include <string.h>
 
-
-#include <stdio.h>
-#include "ft_printf.h"  // Assuming your ft_printf implementation is in ft_printf.h
-
-#include <stdio.h>
-#include "ft_printf.h"  // Assuming your ft_printf implementation is in ft_printf.h
-
-int main() {
-    char *str = "Hello, world!";
-    int num = 42;
-    unsigned int un_num = 100;
-    char ch = 'A';
-
-    // Test string ("%s") with return value
-    int std_ret = printf("Standard printf (string): %s\n", str);
-    int ft_ret = ft_printf("ft_printf (string): %s\n", str);
-    if (std_ret != ft_ret) {
-        printf("Error: Return values differ for string test (std: %d, ft: %d)\n", std_ret, ft_ret);
-    }
-
-    // Test integer ("%d") with return value
-    std_ret = printf("Standard printf (integer): %d\n", num);
-    ft_ret = ft_printf("ft_printf (integer): %d\n", num);
-    if (std_ret != ft_ret) {
-        printf("Error: Return values differ for integer test (std: %d, ft: %d)\n", std_ret, ft_ret);
-    }
-
-    // Test unsigned integer ("%u") with return value
-    std_ret = printf("Standard printf (unsigned integer): %u\n", un_num);
-    ft_ret = ft_printf("ft_printf (unsigned integer): %u\n", un_num);
-    if (std_ret != ft_ret) {
-        printf("Error: Return values differ for unsigned integer test (std: %d, ft: %d)\n", std_ret, ft_ret);
-    }
-
-    // Test character ("%c") with return value
-    std_ret = printf("Standard printf (character): %c\n", ch);
-    ft_ret = ft_printf("ft_printf (character): %c\n", ch);
-    if (std_ret != ft_ret) {
-        printf("Error: Return values differ for character test (std: %d, ft: %d)\n", std_ret, ft_ret);
-    }
-
-    // Test percent sign ("%%") with return value
-    std_ret = printf("Standard printf (%%): %%\n");
-    ft_ret = ft_printf("ft_printf (%%): %%\n");
-    if (std_ret != ft_ret) {
-        printf("Error: Return values differ for percent sign test (std: %d, ft: %d)\n", std_ret, ft_ret);
-    }
-
-    // Mixed format specifier test
-    std_ret = printf("This is a string with a number (%d) and a character (%c).\n", num, ch);
-    ft_ret = ft_printf("This is a string with a number (%d) and a character (%c).\n", num, ch);
-    if (std_ret != ft_ret) {
-        printf("Error: Return values differ for mixed format specifier test (std: %d, ft: %d)\n", std_ret, ft_ret);
-    }
-
-    return 0;
-}
-
 // int main() {
 //     char *str = "Hello, world!";
 //     int num = 42;
 //     unsigned int un_num = 100;
 //     char ch = 'A';
 
-//     // Test string ("%s")
-//     printf("Standard printf (string): %s\n", str);
-//     ft_printf("ft_printf (string): %s\n", str);
+//     // Test string ("%s") with return value
+//     int std_ret = printf("(string): %s\n", str);
+//     int ft_ret = ft_printf("(string): %s\n", str);
+//     if (std_ret != ft_ret) {
+//         printf("Error: Return values differ for string test (std: %d, ft: %d)\n", std_ret, ft_ret);
+//     }
 
-//     // Test integer ("%d")
-//     printf("Standard printf (integer): %d\n", num);
-//     ft_printf("ft_printf (integer): %d\n", num);
+//     // Test integer ("%d") with return value
+//     std_ret = printf("(integer): %d\n", num);
+//     ft_ret = ft_printf("(integer): %d\n", num);
+//     if (std_ret != ft_ret) {
+//         printf("Error: Return values differ for integer test (std: %d, ft: %d)\n", std_ret, ft_ret);
+//     }
 
-//     // Test unsigned integer ("%u")
-//     printf("Standard printf (unsigned integer): %u\n", un_num);
-//     ft_printf("ft_printf (unsigned integer): %u\n", un_num);
+//     // Test unsigned integer ("%u") with return value
+//     std_ret = printf("(unsigned integer): %u\n", un_num);
+//     ft_ret = ft_printf("(unsigned integer): %u\n", un_num);
+//     if (std_ret != ft_ret) {
+//         printf("Error: Return values differ for unsigned integer test (std: %d, ft: %d)\n", std_ret, ft_ret);
+//     }
 
-//     // Test character ("%c")
-//     printf("Standard printf (character): %c\n", ch);
-//     ft_printf("ft_printf (character): %c\n", ch);
+//     // Test character ("%c") with return value
+//     std_ret = printf("(character): %c\n", ch);
+//     ft_ret = ft_printf("(character): %c\n", ch);
+//     if (std_ret != ft_ret) {
+//         printf("Error: Return values differ for character test (std: %d, ft: %d)\n", std_ret, ft_ret);
+//     }
 
-//     // Test percent sign ("%%")
-//     printf("Standard printf (%%): %%\n");
-//     ft_printf("ft_printf (%%): %%\n");
+//     // Test percent sign ("%%") with return value
+//     std_ret = printf("x%");
+//     printf("\n");
+//     ft_ret = ft_printf("x%");
+//     printf("\n");
+    
+//     if (std_ret != ft_ret) {
+//         printf("Error: Return values differ for percent sign test (std: %d, ft: %d)\n", std_ret, ft_ret);
+//     }
+
+//     // Mixed format specifier test
+//     std_ret = printf("This is a string with a number (%d) and a character (%c).\n", num, ch);
+//     ft_ret = ft_printf("This is a string with a number (%d) and a character (%c).\n", num, ch);
+//     if (std_ret != ft_ret) {
+//         printf("Error: Return values differ for mixed format specifier test (std: %d, ft: %d)\n", std_ret, ft_ret);
+//     }
 
 //     return 0;
 // }
@@ -118,3 +85,79 @@ int main() {
 // 	printf("\n---------------------------------------------------------------\n\n\n");
 // 	return (0);
 // }
+
+#include <stdio.h>
+#include "ft_printf.h"  // Assuming your ft_printf implementation is in ft_printf.h
+
+int main() {
+    char *str = "Hello, world!";
+    int num = 42;
+    unsigned int un_num = 100;
+    char ch = 'A';
+    double dbl_num = 3.14159;
+    void *ptr = &str;  // Pointer to a string (for %p)
+
+    // Test characters
+    int std_ret = printf("%c %c\n", ch, 'B');
+    int ft_ret = ft_printf("%c %c\n", ch, 'B');
+    if (std_ret != ft_ret) {
+        printf("Error: Return values differ for characters (std: %d, ft: %d)\n", std_ret, ft_ret);
+    } else {
+        printf("Characters match\n");
+    }
+
+    // Test string ("%s")
+    std_ret = printf("%s\n", str);
+    ft_ret = ft_printf("%s\n", str);
+    if (std_ret != ft_ret) {
+        printf("Error: Return values differ for string (std: %d, ft: %d)\n", std_ret, ft_ret);
+    } else {
+        printf("Strings match\n");
+    }
+
+    // Test pointer ("%p")
+    std_ret = printf("%p\n", ptr);
+    ft_ret = ft_printf("%p\n", ptr);
+    if (std_ret != ft_ret) {
+        printf("Error: Return values differ for pointer (std: %d, ft: %d)\n", std_ret, ft_ret);
+    } else {
+        printf("Pointers match\n");
+    }
+
+    // Test integers ("%d" and "%i")
+    std_ret = printf("%d %i\n", num, num);
+    ft_ret = ft_printf("%d %i\n", num, num);
+    if (std_ret != ft_ret) {
+        printf("Error: Return values differ for integers (std: %d, ft: %d)\n", std_ret, ft_ret);
+    } else {
+        printf("Integers match\n");
+    }
+
+    // Test unsigned integer ("%u")
+    std_ret = printf("%u\n", un_num);
+    ft_ret = ft_printf("%u\n", un_num);
+    if (std_ret != ft_ret) {
+        printf("Error: Return values differ for unsigned integer (std: %d, ft: %d)\n", std_ret, ft_ret);
+    } else {
+        printf("Unsigned integers match\n");
+    }
+
+    // Test hexadecimal ("%x" and "%X")
+    std_ret = printf("%x %X\n", num, num);
+    ft_ret = ft_printf("%x %X\n", num, num);
+    if (std_ret != ft_ret) {
+        printf("Error: Return values differ for hexadecimal (std: %d, ft: %d)\n", std_ret, ft_ret);
+    } else {
+        printf("Hexadecimal values match\n");
+    }
+
+        // Test percent
+    std_ret = printf("x%");
+    ft_ret = ft_printf("x%");
+    if (std_ret != ft_ret) {
+        printf("Error: Return values differ for hexadecimal (std: %d, ft: %d)\n", std_ret, ft_ret);
+    } else {
+        printf("Hexadecimal values match\n");
+    }
+    return 0;
+}
