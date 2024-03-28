efde2057e8b41e5e35536a3fc13b4e5e703f666d
BK Oussama <oussama.boukoutaya@gmail.com>
2024-03-28 18:03:49 +0000

    Update ft_printf.c

--- ft_printf.c
+++ ft_printf.c
@@ -1,42 +1,36 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.c                                        :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ouboukou <ouboukou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/12 15:45:19 by ouboukou          #+#    #+#             */
-/*   Updated: 2024/03/22 02:52:09 by ouboukou         ###   ########.fr       */
+/*   Created: 2024/03/21 22:53:41 by ouboukou          #+#    #+#             */
+/*   Updated: 2024/03/28 18:00:19 by ouboukou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+#include <string.h>
 
-int	ft_printf(const char *format, ...)
+int	main(void)
 {
-	va_list	args;
-	int		index;
-	int		n_printed_chars;
-	size_t	length;
+	int		ft_rslt;
+	int		std_rslt;
+	char	*str;
 
-	va_start(args, format);
-	index = 0;
-	n_printed_chars = 0;
-	length = ft_strlen(format);
-	if (format == NULL || (format[index] == '%' && format[index + 1] == '\0')
-		|| ((format[length] == '%') && (format[length + 1] == '\0')))
-		return (-1);
-	while (format[index])
-	{
-		if (format[index] == '%')
-		{
-			n_printed_chars += ft_conversion(format, args, (index + 1));
-			index++;
-		}
-		else
-			n_printed_chars = n_printed_chars + ft_putchar(format[index]);
-		index++;
-	}
-	va_end(args);
-	return (n_printed_chars);
+	str = NULL;
+	printf("\n----------------------- Standard Output: ----------------------\n");
+	ft_rslt = ft_printf("%");
+	printf("\n");
+	std_rslt = printf("%");
+	printf("\n");
+	printf("\n------------------------ Return Values: -----------------------\n");
+	if (std_rslt != ft_rslt)
+		printf("\nError: Return values do not match\n");
+	else
+		printf("\nReturn values matches\n");
+	printf("std return (Values: %d\nFT_return Value: %d\n", std_rslt, ft_rslt));
+	printf("\n---------------------------------------------------------------\n\n\n");
+	return (0);
 }
